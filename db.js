e1ba2c964fb56ec5de14f332c25270015f3d9460
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-31 16:10:53 +0100

    Defined all Models

--- db.js
+++ db.js
@@ -1,9 +1,16 @@
-const path = require('path');
-const { Sequelize, Model } = require('sequelize');
+const path = require("path");
+const { Sequelize, DataTypes } = require("sequelize");
 
 // TODO - create the new sequelize connection
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+});
+
+db.authenticate();
 
 module.exports = {
-    sequelize,
-    Sequelize
+  sequelize,
+  Sequelize,
+  DataTypes,
 };
