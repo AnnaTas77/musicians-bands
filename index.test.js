0d099d28b823422c248629c3bc1917a1cb61993a
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-31 16:51:36 +0100

    Created tests for the Update Operation of the 3 models

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { sequelize } = require("./db");
 const { Band, Musician, Song } = require("./index");
 
@@ -32,7 +33,7 @@ describe("Band, Musician, and Song Models", () => {
   });
 
   describe("Testing the Song Model", () => {
-    test("can create a Band", async () => {
+    test("can create a Song", async () => {
       const testSong = await Song.create({
         title: "Thunderstruck",
         year: 1990,
@@ -44,19 +45,44 @@ describe("Band, Musician, and Song Models", () => {
     });
   });
 
-  test("can create a Musician", async () => {
-    // TODO - test creating a musician
-    expect("NO TEST").toBe("EXPECTED VALUE HERE");
-  });
+  describe.only("Updating the models", () => {
+    test("can update a Musician", async () => {
+      const testMusician = await Musician.create({
+        name: "Stevie Young",
+        instrument: "guitar",
+      });
+      await testMusician.update(
+        { name: "Malcolm Young" },
+        {
+          where: {
+            name: "Stevie Young",
+          },
+        }
+      );
+      expect(testMusician.name).toBe("Malcolm Young");
+    });
 
-  test("can update a Band", async () => {
-    // TODO - test updating a band
-    expect("NO TEST").toBe("EXPECTED VALUE HERE");
-  });
+    test("can update a Band", async () => {
+      const testBand = await Band.create({ name: "AC/DC", genre: "rock" });
+      await testBand.update(
+        { name: "Rolling Stones" },
+        { where: { name: "AC/DC" } }
+      );
+      expect(testBand.name).toBe("Rolling Stones");
+    });
 
-  test("can update a Musician", async () => {
-    // TODO - test updating a musician
-    expect("NO TEST").toBe("EXPECTED VALUE HERE");
+    test("can update a Song", async () => {
+      const testSong = await Song.create({
+        title: "Thunderstruck",
+        year: 1990,
+        length: 5,
+      });
+      testSong.update(
+        { title: "Sympathy For The Devil" },
+        { where: { title: "Thunderstruck" } }
+      );
+      expect(testSong.title).toBe("Sympathy For The Devil");
+    });
   });
 
   test("can delete a Band", async () => {
