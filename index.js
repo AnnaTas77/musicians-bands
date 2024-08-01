23407d58a147315232712bba4810e48ec9afb239
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-01 17:30:37 +0100

    Completed the second task of the Workshop and tested it

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const { Musician } = require("./models/Musician");
 const { Song } = require("./models/Song");
 // Define associations here
 
+// Task 1 - Day 2 - One-to-Many Associations
 Band.hasMany(Musician);
 Musician.belongsTo(Band);
+
+// Task 2 - Day 2 - Many-to-Many Associations
+
+Song.belongsToMany(Band, { through: "SongBands" });
+Band.belongsToMany(Song, { through: "SongBands" });
 
 module.exports = {
   Band,
--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ const { Band, Musician, Song } = require("./index");
 /**
  * Runs the code prior to all tests
  */
-beforeAll(async () => {
+beforeEach(async () => {
   // the 'sync' method will create tables based on the model class
   // by setting 'force:true' the tables are recreated each time the
   // test suite is run
@@ -133,13 +133,9 @@ describe("Band, Musician, and Song Models - DAY 1", () => {
   });
 });
 
-describe.only("One-to-Many Associations - DAY 2", () => {
+describe("One-to-Many Associations - DAY 2", () => {
   test("Band and Musician should have a one-to-one association", async () => {
     const testBand1 = await Band.create({ name: "AC/DC", genre: "rock" });
-    // const testBand2 = await Band.create({
-    //   name: "Rolling Stones",
-    //   genre: "rock",
-    // });
 
     const allMusicians = await Musician.bulkCreate([
       {
@@ -161,10 +157,137 @@ describe.only("One-to-Many Associations - DAY 2", () => {
     // });
 
     const foundMusicians = await testBand1.getMusicians();
-    console.log(JSON.stringify(foundMusicians, null, 2));
+    // console.log(JSON.stringify(foundMusicians, null, 2));
 
     expect(foundMusicians.length).toBe(2);
-    expect(foundMusicians[0].name).toBe('Malcolm Young')
-    expect(foundMusicians[1].name).toBe('Stevie Young')
+    expect(foundMusicians[0].name).toBe("Malcolm Young");
+    expect(foundMusicians[1].name).toBe("Stevie Young");
+  });
+});
+
+describe("Many-to-Many Associations - DAY 2", () => {
+  test("Song and Band should have a many-to-many association", async () => {
+    const allBands = await Band.bulkCreate([
+      { name: "AC/DC", genre: "rock" },
+      {
+        name: "Rolling Stones",
+        genre: "rock",
+      },
+    ]);
+
+    const allSongs = await Song.bulkCreate([
+      {
+        title: "Thunderstruck",
+        year: 1990,
+        length: 5,
+      },
+      {
+        title: "Highway to Hell",
+        year: 1979,
+        length: 4,
+      },
+      {
+        title: "Sympathy For The Devil",
+        year: 1968,
+        length: 6,
+      },
+    ]);
+
+    // console.log("All Bands: ", JSON.stringify(allBands, null, 2));
+    // console.log("All Songs: ", JSON.stringify(allSongs, null, 2));
+
+    let foundBand = await Band.findOne({ where: { name: "AC/DC" } });
+
+    await foundBand.setSongs(allSongs);
+
+    const band1AllSongs = await foundBand.getSongs();
+
+    // console.log(JSON.stringify(band1AllSongs, null, 2));
+
+    let band = await Band.findByPk(1, { include: Song });
+    let song = await Song.findByPk(1, { include: Band });
+    // console.log("HERE band: ", band.toJSON());
+    // console.log("HERE song: ", song.toJSON());
+    expect(band.Songs[0].id).toBe(1);
+    expect(song.Bands[0].id).toBe(1);
+  });
+
+  test("add multiple songs to a band", async () => {
+    const allBands = await Band.bulkCreate([
+      { name: "AC/DC", genre: "rock" },
+      {
+        name: "Rolling Stones",
+        genre: "rock",
+      },
+    ]);
+
+    const allSongs = await Song.bulkCreate([
+      {
+        title: "Thunderstruck",
+        year: 1990,
+        length: 5,
+      },
+      {
+        title: "Highway to Hell",
+        year: 1979,
+        length: 4,
+      },
+      {
+        title: "Sympathy For The Devil",
+        year: 1968,
+        length: 6,
+      },
+    ]);
+
+    // console.log("All Bands: ", JSON.stringify(allBands, null, 2));
+    // console.log("All Songs: ", JSON.stringify(allSongs, null, 2));
+
+    let foundBand = await Band.findOne({ where: { name: "AC/DC" } });
+    await foundBand.setSongs(allSongs);
+    const bandOneAllSongs = await foundBand.getSongs();
+
+    // console.log("band1AllSongs: ", JSON.stringify(band1AllSongs, null, 2));
+    expect(bandOneAllSongs[0].title).toBe("Thunderstruck");
+    expect(bandOneAllSongs[1].title).toBe("Highway to Hell");
+    expect(bandOneAllSongs[2].title).toBe("Sympathy For The Devil");
+  });
+
+  test("add multiple bands to a song", async () => {
+    const allBands = await Band.bulkCreate([
+      { name: "AC/DC", genre: "rock" },
+      {
+        name: "Rolling Stones",
+        genre: "rock",
+      },
+    ]);
+
+    const allSongs = await Song.bulkCreate([
+      {
+        title: "Thunderstruck",
+        year: 1990,
+        length: 5,
+      },
+      {
+        title: "Highway to Hell",
+        year: 1979,
+        length: 4,
+      },
+      {
+        title: "Sympathy For The Devil",
+        year: 1968,
+        length: 6,
+      },
+    ]);
+
+    // console.log("All Bands: ", JSON.stringify(allBands, null, 2));
+    // console.log("All Songs: ", JSON.stringify(allSongs, null, 2));
+
+    let foundSong = await Song.findOne({ where: { title: "Highway to Hell" } });
+    await foundSong.setBands(allBands);
+    const songOneAllBands = await foundSong.getBands();
+
+    // console.log("songOneAllBands: ", JSON.stringify(songOneAllBands, null, 2));
+    expect(songOneAllBands[0].name).toBe("AC/DC");
+    expect(songOneAllBands[1].name).toBe("Rolling Stones");
   });
 });
